5593df67daf9d475634d8afc3a990cb9678cd3ef
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-04-20 07:28:51 +0600

    learn data type variables syntax

---
+++ dataType.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ int main () {
+    int a, b;
+    printf("enter first number: ");
+    scanf("%d", &a);
+    printf("enter second number: ");
+    scanf("%d", &b);
+    int sum = a + b;
+    printf("the sum is %d\n",  sum);
+
+    return 0;
+ }

348fa00afa601fcd5ac31b8d9954da5bb2041d51
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-06-12 12:20:57 +0600

    learnt constant in c

---
+++ constants.c
@@ -0,0 +1,4 @@
+// If you don't want others (or yourself) to change existing variable values, you can use the const keyword.
+
+const int myNum = 15;  // myNum will always be 15
+myNum = 10;  // error: assignment of read-only variable 'myNum'
